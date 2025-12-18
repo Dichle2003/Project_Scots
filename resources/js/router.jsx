4692e91b5332a2ea2574c5a0883b09396cb3daaf
Dichle2003 <ledich10031965@gmail.com>
2025-12-18 15:33:53 +0700

    login

--- resources/js/router.jsx
+++ resources/js/router.jsx
@@ -1,27 +1,48 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter,Navigate, replace } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home";
 import Login from "./pages/auths/Login";
-
+const isAuthenticated = () =>{
+    const token = localStorage.getItem('token');
+    return token != null && token != "";
+}
+const ProtectedRoute=({children})=>{
+    if(!isAuthenticated()){
+        return <Navigate to='/login' replace/>
+    }
+    return children;
+}
 const router = createBrowserRouter([
-    {
-        element: <AppLayout />, // layout dùng chung
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-                handle: {
-                    title: "Trang chủ",
-                },
-            },
-            {
+     {
                 path: "/login",
                 element: <Login />,
                 handle: {
                     title: "Đăng nhập",
                 },
             },
+    {
+        element:
+      (
+          <ProtectedRoute>
+            {/* layout dùng chung */}
+              <AppLayout />  
+        </ProtectedRoute>
+      ),
+       
+        children: [
+            {
+                path: "/",
+                element: isAuthenticated()? <Home />:<Navigate to="/login" replace/>,
+                handle: {
+                    title: "Trang chủ",
+                },
+            },
+           
         ],
+    },
+    {
+        path: "*",
+        element :<Navigate to='/login' replace/>
     },
 ]);
 
